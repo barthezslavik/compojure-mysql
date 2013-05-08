23d294ebb5e45add8b79bd969419ddd95a01d390
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-05-09 00:52:52 +0300

    cleanup

--- src/compojure_mysql/routes/home.clj
+++ src/compojure_mysql/routes/home.clj
@@ -4,12 +4,6 @@
             [korma.db :as db]
             [compojure-mysql.views.layout :as layout]))
 
-(def db {:classname "com.mysql.jdbc.Driver"
-         :subprotocol "mysql"
-         :subname "//localhost:3306/clojure-learn"
-         :user "root"
-         :password ""})
-
 (db/defdb db
   (db/mysql {:host "localhost"
              :port "3306"
